1b0dd1de678c6400451280303b3cd36ad1d73215
Berkutov.D <berkutov.dmitriy@stemsc.com>
2017-08-14 17:10:39 +0300

    new user on click

--- homework1/src/main.js
+++ homework1/src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import css from '../css/style.css';
 
+// user info
 let TableTr = (str, val) => {
     return (<tr>
                 <td>{str.str}</td>
@@ -10,54 +11,60 @@ let TableTr = (str, val) => {
             </tr>
     )
 };
+
+let id = 0;
+// user block
 let PanelInfo = (props) => {
     return (
-            <div className="panel panel-info">
-                <div className="panel-heading">
-                    <h3 className="panel-title">{props.name}</h3>
-                </div>
-                <div className="panel-body">
-                    <div className="row">
-                        <div>
-                            <div className="col-md-3 col-lg-3 " style={{textAlign:"center"}}>
-                                <img src="http://psdexport.com/storage/avatars/default.png" className="pull-left"/>
-                            </div>
-                            <div className=" col-md-9 col-lg-9">
-                                <table className="table table-user-information">
-                                    <tbody>
-                                        {<TableTr str = "Дата рождения" val = "24.10.2016"/>}
-                                        {<TableTr str = "Пол" val = "MALE"/>}
-                                        {<TableTr str = "Адрес" val = "GOOGLE STREET"/>}
-                                        {<TableTr str = "Email" val = "EMAIL"/>}
-                                    </tbody>
-                                </table>
+            <div>
+                <div    className="panel panel-info">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">{props.name}</h3>
+                    </div>
+                    <div className="panel-body">
+                            <div className="row">
+                            <div>
+                                <div className="col-md-3 col-lg-3 " style={{textAlign:"center"}}>
+                                    <img src="http://psdexport.com/storage/avatars/default.png" className="pull-left"/>
+                                </div>
+                                <div className=" col-md-9 col-lg-9">
+                                    <table className="table table-user-information">
+                                        <tbody>
+                                            {<TableTr str = "Дата рождения" val = "24.10.2016"/>}
+                                            {<TableTr str = "Пол" val = "MALE"/>}
+                                            {<TableTr str = "Адрес" val = "GOOGLE STREET"/>}
+                                            {<TableTr str = "Email" val = "EMAIL"/>}
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div id = {props.val}></div>
             </div>
     )
 };
-
+// button
 let Button = () => {
     return <button type="button" className="button">Add user</button>
 };
 
-const Main = (newUser) => {
-    console.log('newUser.user', newUser.user)
+// render
+const Main = () => {
     return (
         <div>
-            {/* {...newUser.user} */}
-            {<PanelInfo name = "USER FULL NAME !" />}
+            {<PanelInfo name = "USER FULL NAME !" val = {id}/>}
             {<Button />}
         </div>
     )
 };
-
 const mountNode = document.body.querySelector('.app');
-ReactDOM.render(<Main  user = ""/>, mountNode);
+ReactDOM.render(<Main />, mountNode);
 
+// click
 const buttonClick = document.querySelector('.button').addEventListener('click', () => {
-    let users = document.querySelectorAll('.panel.panel-info');
-    ReactDOM.render(<Main  user = {users}/>, mountNode);
+    const newBlock = document.getElementById(id);
+    id++;
+    ReactDOM.render(<PanelInfo  val = {id}/>, newBlock);
 });
